24e05fd85872104a85f8f50dd8de450a0974fdac
HeartLezz7 <heartchantapath@gmail.com>
2024-12-10 15:00:50 +0700

    add health and ping API, add dockerfile, add github workflows

--- main.go
+++ main.go
@@ -10,6 +10,14 @@ func main() {
 	gin.Default()
 	server := gin.New()
 
+	server.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, "pong")
+	})
+
+	server.GET("/health", func(c *gin.Context) {
+		c.JSON(200, "OK")
+	})
+
 	port := os.Getenv("PORT")
 	server.Run(":" + port)
 
