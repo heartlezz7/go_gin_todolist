672b11257cd55bf614699054a1fc9dd553bab287
HeartLezz7 <heartchantapath@gmail.com>
2024-12-11 21:08:55 +0700

    CI docker image test

--- database/database.go
+++ database/database.go
@@ -2,52 +2,49 @@ package database
 
 import (
 	"database/sql"
-	"fmt"
-	"log"
-	"os"
 
 	_ "github.com/lib/pq"
 )
 
 var DB *sql.DB
 
-type postgresConfig struct {
-	user     string
-	password string
-	host     string
-	port     string
-	dbName   string
-	sslmode  string
-}
+// type postgresConfig struct {
+// 	user     string
+// 	password string
+// 	host     string
+// 	port     string
+// 	dbName   string
+// 	sslmode  string
+// }
 
-func ConnectPostgres() error {
+// func ConnectPostgres() error {
 
-	var err error
-	// connect postgres URL
-	var conf = postgresConfig{
-		user:     os.Getenv("DB_USER"),
-		password: os.Getenv("DB_PASSWORD"),
-		host:     os.Getenv("DB_HOST"),
-		port:     os.Getenv("DB_PORT"),
-		dbName:   os.Getenv("DB_NAME"),
-		sslmode:  os.Getenv("DB_MODE"),
-	}
+// 	var err error
+// 	// connect postgres URL
+// 	var conf = postgresConfig{
+// 		user:     os.Getenv("DB_USER"),
+// 		password: os.Getenv("DB_PASSWORD"),
+// 		host:     os.Getenv("DB_HOST"),
+// 		port:     os.Getenv("DB_PORT"),
+// 		dbName:   os.Getenv("DB_NAME"),
+// 		sslmode:  os.Getenv("DB_MODE"),
+// 	}
 
-	// set postgres URL
-	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", conf.user, conf.password, conf.host, conf.port, conf.dbName, conf.sslmode)
+// 	// set postgres URL
+// 	dbURL := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", conf.user, conf.password, conf.host, conf.port, conf.dbName, conf.sslmode)
 
-	DB, err = sql.Open("postgres", dbURL)
+// 	DB, err = sql.Open("postgres", dbURL)
 
-	if err != nil {
-		log.Fatal("can not connect to database;", err)
-	}
+// 	if err != nil {
+// 		log.Fatal("can not connect to database;", err)
+// 	}
 
-	// ping postgres DB
-	if err = DB.Ping(); err != nil {
-		log.Fatal("ping: ", err)
-		return err
-	}
+// 	// ping postgres DB
+// 	if err = DB.Ping(); err != nil {
+// 		log.Fatal("ping: ", err)
+// 		return err
+// 	}
 
-	fmt.Println("Successfully connected to postgres DB")
-	return nil
-}
+// 	fmt.Println("Successfully connected to postgres DB")
+// 	return nil
+// }
--- server/server.go
+++ server/server.go
@@ -2,9 +2,10 @@ package server
 
 import (
 	"os"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/heartlezz7/go_gin_todolist/database"
 	"github.com/heartlezz7/go_gin_todolist/model"
 	"github.com/heartlezz7/go_gin_todolist/router"
 )
@@ -16,11 +17,21 @@ func RunServer() error {
 
 	server := model.Server{Engine: engine}
 
-	err = database.ConnectPostgres()
-	if err != nil {
-		return err
-	}
-	defer database.DB.Close()
+	server.Engine.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},                                       // Specify allowed origins
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},   // Specify allowed HTTP methods
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"}, // Specify allowed headers
+		ExposeHeaders:    []string{"Content-Length"},                          // Specify headers to expose
+		AllowCredentials: true,                                                // Allow credentials (cookies, authorization headers, etc.)
+		MaxAge:           12 * time.Hour,                                      // Specify the duration for which preflight requests can be cached
+	}))
+
+	// connect DB
+	// err = database.ConnectPostgres()
+	// if err != nil {
+	// 	return err
+	// }
+	// defer database.DB.Close()
 
 	router.Init(server)
 
